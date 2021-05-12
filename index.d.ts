39b600975f629612c766a58d251ec4200931a2cc
Maxwell Olliver <maxwellmacedo2015@gmail.com>
2021-05-12 18:03:07 -0300

    Feature: Change to commonjs

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,25 @@
+export function classNames(
+  arg1: string | { [key: string]: any },
+  arg2?: { [key: string]: any }
+  ): string
+  
+export function copyText(text: string): boolean
+
+export function debounce(fn: () => any, wait: number): () => any
+
+export function escapeRegex(regexToEscape: string): string
+  
+export function fileExtName(fileName: string): string
+
+export function formatPhone(phone: string): string
+
+export function generateGoogleCalendarLink(calendarObj: GoogleCalendarObj): string
+
+export function isObjEmpty(obj: {[key: string]: any}): boolean
+
+export function pad(str: string, length: number, insertOn: 'right' | 'left'): string
+
+export function randomHexColor(): string
 export interface GoogleCalendarObj {
   text: string
   location: string
@@ -7,32 +29,3 @@ export interface GoogleCalendarObj {
   }
   details: string
 }
-
-export function addUtilDays(date: Date, daysToAdd: number, onlyOnFinal: boolean): Date
-
-export function classNames(
-  arg1: string | { [key: string]: any },
-  arg2?: { [key: string]: any }
-): string
-
-export function copyText(text: string): boolean
-
-export function debounce(fn: () => any, wait: number): () => any
-
-export function escapeRegex(regexToEscape: string): string
-
-export function fileExtName(fileName: string): string
-
-export function flexFormat(date: Date, output: 'DMY' | 'YMD'): Date
-
-export function formatPhone(phone: string): string
-
-export function generateGoogleCalendarLink(calendarObj: GoogleCalendarObj): string
-
-export function isAuthenticated(key: string): boolean
-
-export function isObjEmpty(obj: {[key: string]: any}): boolean
-
-export function pad(str: string, length: number, insertOn: 'right' | 'left'): string
-
-export function randomHexColor(): string
